bfb1332bd4685922227f395c41dc81ecfaa257eb
Wayne Estrera <estrerawayne@gmail.com>
2024-07-04 11:02:06 +0800

    pushed section-18

---
+++ Section 18 - Advanced JavaScript and DOM Manipulation/zip/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+var num = document.querySelectorAll(".drum").length;
+for (var i = 0; i < num; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked! " + i + "x");
+  });
+}
