a6006d500252d999d73d543153fe1aa0b5f0f246
Wayne Estrera <estrerawayne@gmail.com>
2024-08-09 14:42:02 +0800

    update section 16

--- Section 16 - The Document Object Model/exercise-2/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ Section 16 - The Document Object Model/exercise-2/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,22 @@
-document.query
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "Good Bye";
+
+// heading.style.color = "red";
+
+// inputButton = document.querySelector("input");
+
+// inputButton.click();
+
+// document.getElementById("li");
+
+//! this doesn't work
+// document.getElementById("li").style.color = "purple";
+//* use this instead if you want to change the color to an element with multiple child elements.
+//* you need to add square brackets and set the index to access specific elements.
+
+// document.getElementsByTagName("li").length;
+// document.getElementById("title").innerHTML = "Get the fuck out of here";
+// document.getElementsByClassName("btn")[0].style.color = "red";
+// document.getElementById("li")[2].style.color = "purple";
+
+document.querySelector("li a");
